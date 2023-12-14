407b548a17b6fb14311ebf6b6f7dda99a2ea9324
HRIDOY CHANDRA  SAHA <145109607+hridoywebdev12@users.noreply.github.com>
2023-12-13 23:55:03 -0800

    S

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Shard/Header/Header';
+import Banner from './banner/Banne';
+
+
+
+const Home = () => {
+          return (
+                    <div>
+                    
+                    <Header></Header>
+                    <Banner/>
+
+                    </div>
+          );
+};
+
+export default Home;
